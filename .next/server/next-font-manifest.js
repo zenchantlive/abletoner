098685acf1e7846c48b727482dee09dd61043db3
zenchantlive <jordanlive121@gmail.com>
2023-05-26 20:16:51 -0700

    g -message "first commit"

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/560d0dc937e89811-s.p.woff2"
+    ]
+  },
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": true
+}
